87965d2ace6ce0304558fc6d826b2e68f4d9fc2e
bunicholas <nicholascarroll123@gmail.com>
2022-04-13 19:00:03 -0400

    M

---
+++ week6/server.js
@@ -0,0 +1,65 @@
+import './App.js'
+var express = require('express')
+var mysql = require('mysql2')
+const bodyParser = require('body-parser');
+var db = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : 'root',
+    database : 'capstone'
+});
+
+db.connect((err) => {
+    if (err){
+        throw err;
+    }
+    console.log("MySQL Database is connected Succesfully!");
+});
+const app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get('/people',(req,res) => {
+let sqlString = "Select * FROM people;"
+db.query(sqlString, (err, result) => {
+if (err){
+    throw err;
+}
+console.log(result);
+res.send(result)
+});
+});
+app.post('/addInstructor/:name',(req,res) => {
+    let sqlString = `INSERT INTO people (instructors) VALUES ('${req.params.name}')`;
+    db.query(sqlString, (err, result) => {
+    if (err){
+        throw err;
+    }
+    console.log(result);
+    res.send(result)
+    });
+    });
+app.put('/update',(req,res) => {
+        let sqlString = `UPDATE people SET instructors = ${req.params.name};`
+        db.query(sqlString, (err, result) => {
+        if (err){
+            throw err;
+        }
+        console.log(result);
+        res.send(result)
+        });
+        });
+app.delete('/delete',(req,res) => {
+            let sqlString = `DROP TABLE people;`
+            db.query(sqlString, (err, result) => {
+            if (err){
+                throw err;
+            }
+            console.log(result);
+            res.send(result)
+            });
+            });           
+app.listen(4000, () => {
+    console.log("The server is running on Port 4000")
+})
---
+++ week7/capstone/src/App.js
@@ -0,0 +1,24 @@
+import './App.css'
+import './server.js'
+import React from 'react';
+import { useState } from 'react';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
